6aa812d0158300d4af5596035828a73048586154
Suneil England <kxnglish@gmail.com>
2025-10-16 22:08:33 -0500

    next.config.js updated

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: { domains: ["res.cloudinary.com"] },
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   experimental: { esmExternals: false },
+
+//   // ✅ Unblock CI on lint
+//   eslint: { ignoreDuringBuilds: true },
+
+//   // ✅ Unblock CI on TS type errors (if any)
+//   typescript: { ignoreBuildErrors: true },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ["res.cloudinary.com"] },
@@ -5,11 +21,8 @@ const nextConfig = {
   swcMinify: true,
   experimental: { esmExternals: false },
 
-  // ✅ Unblock CI on lint
+  // Unblock CI on lint + TS type errors
   eslint: { ignoreDuringBuilds: true },
-
-  // ✅ Unblock CI on TS type errors (if any)
   typescript: { ignoreBuildErrors: true },
 };
-
 module.exports = nextConfig;
