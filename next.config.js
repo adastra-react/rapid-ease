de672e92e95f6c23f646605913e0b441916748e6
Suneil England <kxnglish@gmail.com>
2025-10-18 21:33:41 -0500

    new next config

--- next.config.js
+++ next.config.js
@@ -29,24 +29,19 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["res.cloudinary.com"] },
+  images: {
+    unoptimized: true,
+    domains: ["res.cloudinary.com"],
+  },
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     esmExternals: false,
-    missingSuspenseWithCSRBailout: false,
   },
 
   // Unblock CI on ALL errors
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-
-  // This is the key - it will export despite errors
-  output: "export",
-  distDir: ".next",
-
-  // Override the page extension handler to ignore metadata errors
-  pageExtensions: ["tsx", "ts", "jsx", "js"],
 };
 
 module.exports = nextConfig;
